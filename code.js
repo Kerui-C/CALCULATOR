82c292efd7477838404e54a7c5f7266f93545ec6
Kerui-C <keruichen2@gmail.com>
2023-12-31 18:51:26 -0500

    yes

---
+++ code.js
@@ -0,0 +1,15 @@
+const add = function (a, b) {
+    return a + b;
+};
+  
+const subtract = function (a, b) {
+    return a - b;
+};
+
+const mul = function (a, b) {
+    return a - b;
+};
+
+const division = function (a, b) {
+    return a - b;
+};
